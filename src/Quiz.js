05d46243a52260602d5c9287f396adc959f91e60
Rodriguez Ortiz, D <d.rodriguez.ortiz@accenture.com>
2020-04-25 10:35:30 -0600

    Quiz file created.

---
+++ src/Quiz.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+let quizData = require('./quiz_data.json')
+
+class Quiz extends Component {
+    render () {
+      <div>
+       Quiz
+      </div>
+    }
+    constructor(props) {
+        super(props);
+        // Don't call this.setState() here!
+        this.state = { quiz_position: 1 };
+        // this.handleClick = this.handleClick.bind(this);
+      }
+  }
+
+  export default Quiz
