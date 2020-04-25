9def756e2c6c58285b781d011003b8d9a7c4064d
Rodriguez Ortiz, D <d.rodriguez.ortiz@accenture.com>
2020-04-25 10:46:13 -0600

    Quiz question

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,18 +3,21 @@ import React, { Component } from 'react';
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
-    render () {
-      <div>
-       Quiz
-      </div>
-    }
+
     constructor(props) {
         super(props);
-        // Don't call this.setState() here!
         this.state = { quiz_position: 1 };
-        // this.handleClick = this.handleClick.bind(this);
       }
+
+    render () {
+        return(
+         <div>
+            <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
+         </div>
+        )
+      
+    }
+    
   }
 
   export default Quiz;
-  
