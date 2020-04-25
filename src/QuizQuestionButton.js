63a1248f0b8b74a60dad6738b3c559e7463de987
Rodriguez Ortiz, D <d.rodriguez.ortiz@accenture.com>
2020-04-25 11:13:38 -0600

    Quiz question button class create

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class QuizQuestionButton extends Component {
+    render() {
+        <li>
+            <button>
+                
+            </button>
+        </li>
+    }
+
+}
+
+export default QuizQuestionButton
